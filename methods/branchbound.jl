c66a359e8da7f7d5498000568efbc2439f5a905d
Martina Maggio <maggio.martina@gmail.com>
2020-03-16 20:50:45 +0100

    fixing error in computation of p for branch and bound

--- methods/branchbound.jl
+++ methods/branchbound.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 using IterTools
 
-function jsr_branchbound(v; delta=1e-4, max_iter=10)
+function jsr_branchbound(v; delta=1e-4, max_iter=500)
 
   # ---------------------------------------------------------------- #
   # this function computes bounds on the joint spectral radius using #
@@ -13,7 +13,7 @@ function jsr_branchbound(v; delta=1e-4, max_iter=10)
 
   spectral_radiuses(T) = map(x -> maximum(abs.(eigvals(x))), T)
   norms(T) = map(x -> norm(x), T)
-  product(T) = reduce((x,y) -> x*y, T, init=I)
+  iter_mu(M, P, val, cur_iter) = min(val, norm(M*P)^(1/cur_iter))
 
   # iteration 1
   sp1 = spectral_radiuses(v)
@@ -23,6 +23,7 @@ function jsr_branchbound(v; delta=1e-4, max_iter=10)
   
   # branch and bound
   To = copy(v) # initializing the set of the previous iteration
+  muo = copy(nm1) # initializing the set of mu values
   alphan = alpha1 # initializing the bounds for the previous iteration
   betan = beta1
   
@@ -34,23 +35,29 @@ function jsr_branchbound(v; delta=1e-4, max_iter=10)
     # set for the (n-1)-th iteration in To, and the initial set v
     # note: we need to multiply an entry in v with an entry in To
     Tn = Matrix{Float64}[]
-    for x in Iterators.product(v, To)
+    mun = Float64[] # \mu at iteration n
+    for x in Iterators.product(1:length(v), 1:length(To))
       # we are going to compute p(candidate) and add it to the list
       # for the next iteration only if it is greater than the current
       # alpha + delta, if Tn is empty then we can't improve with
       # branch and bound and we should exit and return the bounds we
       # found - alphan and betan
-      p = min(norm(x[1]), norm(x[1]*x[2])^(1/2))
-      if (p > alphan + delta) push!(Tn, x[1]*x[2]) end
+      idx_v = x[1]
+      idx_To = x[2]
+      p_candidate = iter_mu(v[idx_v], To[idx_To], muo[idx_To], num_iter)
+      if (p_candidate > alphan + delta)
+        push!(Tn, v[idx_v]*To[idx_To])
+        push!(mun, p_candidate)
+      end
     end
     
     if isempty(Tn) break end # get out if we can't improve
     
     sp_pow = map(x -> x^(1/num_iter), spectral_radiuses(Tn))
-    p = [minimum(norm(product(Tn[1:idx]))^(1/idx)) for idx = 1:length(Tn)]
-    betan = min(betan, max(alphan + delta, maximum(p)))
+    betan = min(betan, max(alphan + delta, maximum(mun)))
     alphan = max(alphan, maximum(sp_pow))
-    To = Tn # preparing for the next iteration
+    To = copy(Tn) # preparing for the next iteration
+    muo = copy(mun)
     
   end
   
